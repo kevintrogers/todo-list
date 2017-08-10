8f39b6d8e9a22569fad4dc0a336ebab9634a3072
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-08-10 05:44:16 +0000

    enabled toggle of private tasks

--- todo-list/imports/ui/task.js
+++ todo-list/imports/ui/task.js
@@ -27,10 +27,13 @@ if (Meteor.isClient) {
 
 
 Template.task.events({
+  'click .toggle-private'() {
+    // Set the checked property to the opposite of its current value
+   Meteor.call('tasks.setPrivate', this._id, !this.private);
+  },
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
    Meteor.call('tasks.setChecked', this._id, !this.checked);
-  
   },
   'click .delete'() {
     Meteor.call('tasks.remove', this._id);
